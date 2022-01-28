ed3c941bfe8fb4ff8ac9f075d7926089f6e3b0a9
ahesse2567 <67125718+ahesse2567@users.noreply.github.com>
2022-01-28 14:31:35 -0600

    Update prepare_threshold_data.R

--- prepare_threshold_data.R
+++ prepare_threshold_data.R
@@ -8,3 +8,30 @@ vts <- vts %>%
          at = lubridate::hour(at) + (lubridate::minute(at)/60)) %>% 
   mutate(rc = lubridate::hms(rc),
          rc = lubridate::hour(rc) + (lubridate::minute(rc)/60))
+
+test <- read_csv("data/processed/stages/mar22_105_pre.csv")
+
+mar22_105 <- vts[1,]
+
+test$increment <- mar22_105$speed_2mi
+
+fixed_speeds <- case_when(test$stage == 1 ~ 3,
+          test$stage == 2 ~ round(mar22_105$speed_2mi*.75,1),
+          test$stage == 3 ~ round(mar22_105$speed_2mi*0.3,1),
+          test$stage == 4 ~ round(mar22_105$speed_2mi*0.635,1),
+          test$stage == 5 ~ round(mar22_105$speed_2mi*0.45,1)
+          )
+fixed_speeds
+
+
+
+stages <- 1:61
+
+numerator <- (mar22_105$speed_2mi - mar22_105$speed_2mi*0.45)
+denominator <- num_stages
+
+numerator / denominator
+
+round(numerator / denominator * (stages - 5) + mar22_105$speed_2mi*0.45, 1)
+
+round(stage15 + mar22_105$speed_2mi*0.45, 1)
