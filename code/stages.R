c40b69df3999aef2ddbc913ef47c39c26fdaca71
ahesse2567 <67125718+ahesse2567@users.noreply.github.com>
2022-01-28 13:21:41 -0600

    Added stage column

--- code/stages.R
+++ code/stages.R
@@ -2,24 +2,7 @@ library(tidyverse)
 
 rm(list = ls())
 
-pre_ramp_stage_lengths <- c(60, 60*5, rep(60*3, 3))
-ramp_stage_lengths <- rep(15, 61 - length(pre_ramp_stage_lengths))
-
-stage_lengths <- c(pre_ramp_stage_lengths, ramp_stage_lengths)
-
-stages <- tibble(stage = 1:61,
-                 stage_length = stage_lengths)
-
-stages <- stages %>% 
-  mutate(time_start = cumsum(stage_lengths) - stage_lengths[1],
-         time_end = cumsum(stage_lengths)) %>% 
-  mutate(stage_length = stage_length/60,
-         time_start = time_start/60,
-         time_end = time_start/60)
-
-test <- read_csv("data/processed/mar22_105_pre.csv")
-
-get_stage <- function(.x) {
+add_stages <- function(.df, time_col = "ex_time") {
   # make stages first
   pre_ramp_stage_lengths <- c(60, 60*5, rep(60*3, 3))
   ramp_stage_lengths <- rep(15, 61 - length(pre_ramp_stage_lengths))
@@ -35,20 +18,33 @@ get_stage <- function(.x) {
     mutate(stage_length = stage_length/60,
            time_start = time_start/60,
            time_end = time_end/60)
-  ts <- stages[["time_start"]] - .x
-  ts <- stages[["time_end"]] - .x
+  # browser()
+  stage_num <- integer(length = nrow(.df))
+  for(i in 1:nrow(.df)) {
+    idx <- which((.df[[time_col]][i] >= stages$time_start) & 
+                   (.df[[time_col]][i] < stages$time_end))
+    stage_num[i] <- stages$stage[idx]
+  }
+  
+  .df <- .df %>% 
+    mutate(stage = stage_num)
+  
+  .df
   
 }
 
-(test$ex_time >= stages$time_start) & (test$ex_time < stages$time_end)
+file_list <- list.files("data/processed/no_events", full.names = TRUE)
 
-stage_num <- integer(length = nrow(test))
-for(i in 1:nrow(test)) {
-  idx <- which((test$ex_time[i] >= stages$time_start) & 
-                 (test$ex_time[i] < stages$time_end))
-  stage_num[i] <- stages$stage[idx]
+test_list <- map(file_list, .f = function(x) {read_csv(x) %>% 
+    drop_na(ex_time)})
+
+test_list_stages <- vector(mode = "list", length = length(test_list))
+
+for(i in 1:length(test_list_stages)) {
+  test_list_stages[[i]] <- add_stages(test_list[[i]])
+  test_name <- str_replace(file_list[i], "data/processed//", "")
+  write_csv(test_list_stages[[i]], file = paste0("data/processed/stages/",
+                                                 test_name))
 }
 
-test %>% 
-  mutate(stage = stage_num) %>% 
-  select(speed, grade, vo2, stage, ex_time) %>% View
+
