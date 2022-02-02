045b34856be000b52bd1279b75f9a10251d6c100
Anton Hesse <ahesse2567@gmail.com>
2022-02-01 21:35:51 -0600

    mrt calculated for each person

--- code/ramp_rate.R
+++ code/ramp_rate.R
@@ -29,7 +29,7 @@ get_mrt <- function(.data, id, vt_data, front_cutoff = 1) {
   
   .data <- .data %>% 
     mutate(ramp_time = ex_time - min_ramp_time,
-           ramp_speed = ramp_rate * ramp_time + start_speed)
+           ramp_speed = ramp_rate * ramp_time + rec2_speed)
   
   mrt_vo2 <- .data %>% 
     filter(stage == 4) %>% 
@@ -45,7 +45,7 @@ get_mrt <- function(.data, id, vt_data, front_cutoff = 1) {
   
   mod <- lm(vo2 ~ ramp_speed + 1, data = mrt_ramp_section)
   
-  mrt_speed <- test %>% 
+  mrt_speed <- .data %>% 
     filter(stage == 4) %>% 
     select(fixed_speeds) %>% 
     summarize(mrt_speed = get_mode(fixed_speeds)) %>% 
@@ -59,122 +59,38 @@ get_mrt <- function(.data, id, vt_data, front_cutoff = 1) {
   return(mrt)
 }
 
-test_raw <- read_csv("data/processed/fixed_speeds_grades/mar22_105_pre.csv")
-test <- avg_exercise_test(test_raw, type = "breath", subtype = "rolling",
-                          time_col = "ex_time", roll_window = 7, trim = 2)
+file_list <- list.files("data/processed/fixed_speeds_grades/",
+                        full.names = TRUE)
 
-get_mrt(test, id = 105, vt_data = vts, front_cutoff = 1)
+test_list_raw <- vector(mod = "list", length = length(file_list))
 
-# technically start_speed should be the speed at the second recovery pace
-# but it seems to give answers that are too right shifted
-rec2_speed <- test_raw %>% 
-  filter(stage == 5) %>% 
-  select(fixed_speeds) %>% 
-  summarize(start_speed = get_mode(fixed_speeds)) %>% 
-  pull()
+for(i in 1:length(file_list)) {
+  test_list_raw[[i]] <- read_csv(file_list[i])
+}
 
-ramp_start_speed <- test_raw %>% 
-  filter(stage == 6) %>% 
-  select(fixed_speeds) %>% 
-  summarize(start_speed = get_mode(fixed_speeds)) %>% 
-  pull()
+test_list <- vector(mod = "list", length = length(test_list_raw))
 
-# start_speed <- mean(c(rec2_speed, ramp_start_speed))
-start_speed <- rec2_speed
+test_list <- map(test_list_raw, avg_exercise_test,
+    type = "breath",
+    subtype = "rolling",
+    time_col = "ex_time",
+    roll_window = 7,
+    trim = 2)
 
-max_speed <- max(test_raw$fixed_speeds)
+ids <- vts$id
+mrt_data <- numeric(length = length(ids))
 
-ramp_rate <- (max_speed - rec2_speed) / 8 # ramp lasts 8 minutes
+for(i in 1:length(test_list_raw)) {
+  mrt_data[i] <- get_mrt(.data = test_list[[i]],
+                          id = ids[i],
+                          vt_data = vts,
+                          front_cutoff = 1)
+}
 
-test <- avg_exercise_test(test_raw, type = "breath", subtype = "rolling",
-                  time_col = "ex_time", roll_window = 7, trim = 2)
+mrt_data
 
-test <- test %>% 
-  mutate(phase = case_when(stage == 1 ~ "warm-up",
-                           stage == 2 ~ "marathon_pace_ss",
-                           stage == 3 ~ "rec1",
-                           stage == 4 ~ "mrt_ss",
-                           stage == 5 ~ "rec2",
-                           stage >= 6 ~ "ramp"))
+mrt_tib <- tibble(id = ids, time_point = "pre", mrt = mrt_data)
+mrt_tib
 
-min_ramp_time <- test %>% 
-  filter(phase == "ramp") %>% 
-  select(ex_time) %>% 
-  min()
-
-test <- test %>% 
-  mutate(ramp_time = ex_time - min_ramp_time,
-         ramp_speed = ramp_rate * ramp_time + start_speed)
-
-ggplot(data = test[test["stage"] >= 5,],
-       aes(x = ex_time, y = fixed_speeds)) +
-  geom_point(aes(color = grade)) +
-  geom_line(aes(y = ramp_speed)) +
-  theme_bw()
-
-# test %>% 
-#   filter(stage >= 5) %>% 
-#   View
-
-mrt_vo2 <- test %>% 
-  filter(phase == "mrt_ss") %>% 
-  filter(ex_time >= max(ex_time) - 0.5) %>% 
-  summarize(avg_vo2 = mean(vo2)) %>% 
-  pull()
-
-vts_raw <- read_csv("./data/threshold_data.csv", show_col_types = FALSE) %>% 
-  drop_na() #some people ran the two mile but didn't complete an exercise test
-
-#cleans up the times in the spreadsheet with AT and RC data
-vts <- vts_raw %>% 
-  mutate(at = lubridate::hms(at),
-         at = lubridate::hour(at) + (lubridate::minute(at)/60)) %>% 
-  mutate(rc = lubridate::hms(rc),
-         rc = lubridate::hour(rc) + (lubridate::minute(rc)/60))
-
-mar22_105_pre <- vts[vts["id"] == 105,]
-
-mrt_ramp_105_pre <- test %>% 
-  filter(ex_time >= min_ramp_time & ex_time < mar22_105_pre$at)
-
-# remove first minute or ramp data b/c they're probably walking
-lm_data <- mrt_ramp_105_pre[mrt_ramp_105_pre["ramp_time"] >= 1,]
-
-mod <- lm(vo2 ~ ramp_speed + 1, data = lm_data)
-
-mrt_speed <- test %>% 
-  filter(phase == "mrt_ss") %>% 
-  select(fixed_speeds) %>% 
-  summarize(mrt_speed = get_mode(fixed_speeds)) %>% 
-  pull()
-
-ramp_speed_mrt_vo2 <- (mrt_vo2 - mod$coefficients[1]) / mod$coefficients[2]
-speed_diff <- ramp_speed_mrt_vo2 - mrt_speed
-
-mrt <- (speed_diff / ramp_rate)
-mrt
-
-vt1_mrt_ex_time <- mar22_105_pre$at - mrt
-vt1_mrt_idx <- which.min(abs(test$ex_time - vt1_mrt_ex_time))
-
-speed_vt1^-1*60
-round(((speed_vt1^-1*60) %% 1)* 60,0)
-
-ggplot(data = mrt_ramp_105_pre, aes(x = ramp_speed, y = vo2)) +
-  geom_point() +
-  geom_hline(yintercept = mrt_vo2, color = "red") +
-  geom_vline(xintercept = mrt_speed, color = "red") +
-  geom_vline(xintercept = ramp_speed_mrt_vo2) +
-  geom_vline(xintercept = speed_vt1, color = "green") +
-  geom_smooth(data= lm_data, method = "lm", se = FALSE) +
-  geom_text(x = quantile(mrt_ramp_105_pre$ramp_speed, 0.2),
-            y = quantile(mrt_ramp_105_pre$vo2, 0.6),
-            label = paste("Speed at VT1:",
-                          speed_vt1,
-                          "mph\nPace at VT1:",
-                          round(speed_vt1^-1*60,0),
-                          "min",
-                          round(((speed_vt1^-1*60) %% 1)* 60,0),
-                          "sec/mi")) +
-  theme_bw()
+write_csv(mrt_tib, "data/processed/mrt.csv")
 
