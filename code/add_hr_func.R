0efec4b15dc3c2065c776abeb18a3becc8172529
Anton Hesse <ahesse2567@gmail.com>
2022-02-04 11:48:18 -0600

    Create add_hr_func.R

---
+++ code/add_hr_func.R
@@ -0,0 +1,46 @@
+bind_hr_data <- function(id, time_point) {
+  id <- as.character(id)
+  browser()
+  file_list_hrv <- list.files('data/hrv/')
+  
+  f_name_hrv <- file_list_hrv[str_which(paste0("./data/hrv/", id, ".txt"))]
+  hr_dat <- read_delim(file = f_name_hrv,
+                       delim = "/t",
+                       col_names = FALSE,
+                       show_col_types = FALSE) %>% 
+    rename(rr = X1)
+  
+  clock_start <- str_extract(f_name_hrv, pattern = "\\s\\d{2}-\\d{2}-\\d{2}")
+  clock_start <- str_remove(clock_start, pattern = "\\s")
+  
+  clock_hr <- str_split(clock_start, "-")[[1]][[1]] %>% as.numeric()
+  clock_min <- str_split(clock_start, "-")[[1]][[2]] %>% as.numeric()
+  clock_sec <- str_split(clock_start, "-")[[1]][[3]] %>% as.numeric()
+  
+  clock_time <- hms(clock_sec, clock_min, clock_hr)
+  
+  hr_data <- hr_data %>% 
+    mutate(elapsed_time = cumsum(rr)/1000, 
+           elapsed_time = hms(seconds = elapsed_time), 
+           clock_time = as_hms(clock_time + elapsed_time), #convert from difftime to hms
+           clock_time = round(clock_time, 0), #round times to allow merging with gas exchange data
+           clock_time = as_hms(clock_time), #convert from difftime to hms
+           hr = 60/(rr/1000)) %>% 
+    group_by(clock_time) %>% #Heart beats can occur multiple times per second, but that's more frequent than data in Breeze
+    summarise(clock_time = mean(clock_time), hr = mean(hr), elapsed_time = mean(elapsed_time)) %>% #Average data down to allow for merge with Breeze
+    mutate(clock_time = as_hms(clock_time)) #switch clock_time back into hms
+  
+  test_list <- list.files("data/processed/fixed_speeds_grades/",
+                          full.names = TRUE)
+  id_tp <- paste0(id, "_", time_point)
+  test <- test_list[str_which(test_list, id_tp)]
+  
+  # Merges data frames and keeps all gas exchange data
+  merge(test, hr_data, by = "clock_time", all.x = TRUE) %>% 
+    as_tibble() %>% 
+    ggplot(data = .) + 
+    geom_point(aes(x = ex_time, y = hr/max(hr)), color = "red") +
+    geom_point(aes(x = ex_time, y = vo2/max(vo2)), color = "blue") +
+    geom_point(aes(x = ex_time, y = speed/max(speed)), color = "green")
+  
+}
