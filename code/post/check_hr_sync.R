139e1dfdcce0100f642bce2787c0f53330ea09e4
Anton Hesse <ahesse2567@gmail.com>
2022-11-16 16:29:24 -0600

    Another hr sync to check

--- code/post/check_hr_sync.R
+++ code/post/check_hr_sync.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+rm(list = ls())
 
 cpet_hr_files <- list.files("data/2022/post/processed/cpet_hr/",
                             full.names = TRUE)
@@ -9,19 +10,6 @@ file_names <- basename(cpet_hr_files) %>%
   tools::file_path_sans_ext()
 
 names(test_list) <- file_names
-
-test_list[["mar22_105_post"]] %>% 
-  ggplot(aes(x = clock_time, y = hr)) +
-  geom_point()
-
-ggplot(data = test_list[["mar22_105_post"]],
-       aes(x = clock_time, y = hr/max(hr, na.rm = TRUE))) +
-  geom_point() +
-  geom_line(aes(y = fixed_speeds/max(fixed_speeds, na.rm = TRUE)), color = "green") +
-  geom_line(aes(y = fixed_grades/max(fixed_grades, na.rm = TRUE)), color = "orange") +
-  theme_minimal() +
-  ggtitle(label = names(test_list["mar22_105_post"]))
-
 
 plot_list = vector(mode = "list", length = length(test_list))
 for (i in 1:length(test_list)) {
